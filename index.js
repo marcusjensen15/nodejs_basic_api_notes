5d9c4b93515ce7ca768e2bbf497fe732b7820438
Marcus Jensen <marcusjensen1515@gmail.com>
2020-10-13 19:44:42 -0700

    add a couple more routes

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const express = require('express');
 const app = express();
 // the app constant represents our application
 
+const courses = [
+  { id:1, name: 'bill'},
+  { id:2, name: "cliff"},
+  { id:3, name: "biff"}
+]
+
 
 app.get('/', (req, res) => {
     res.send('hello world!!!');
 });
 
 app.get('/api/courses', (req,res) => {
-    res.send('sup piggy');
+    res.send(courses);
 });
 
 // :id could be literally anything. just needs to start with ac colon. params is a keyword.
 app.get('/api/courses/:id', (req,res) => {
-  res.send(req.params.id);
+  let course = courses.find( c => c.id === parseInt(req.params.id));
+  //respond with 404 (not found)
+  if(!course) res.status(404).send('the course with the given id was not found');
+  res.send(course);
 });
 
 //you can also add multiple paramaters. here i'm just returning the 'day' value.
