04f529c759ba51d653dab34686a5b78f4f2053d6
Marcus Jensen <marcusjensen1515@gmail.com>
2020-10-13 19:33:09 -0700

    add query param notes

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world!!!');
 });
 
 app.get('/api/courses', (req,res) => {
     res.send('sup piggy');
 });
 
-app.listen(3000, () => console.log('listnening on port 3000'));
+// :id could be literally anything. just needs to start with ac colon. params is a keyword.
+app.get('/api/courses/:id', (req,res) => {
+  res.send(req.params.id);
+});
+
+//you can also add multiple paramaters. here i'm just returning the 'day' value.
+
+app.get('/test/:month/:day', (req,res) => {
+  res.send(req.params.day);
+});
+
+//you can also provide query string parameters (beginning with a ?)
+
+//if you type in localhost:5000/howdy/545/2332/sortBy=name it will return {"sortBy": "name"}
+
+app.get('/howdy/:id/:secondaryid', (req,res) => {
+  res.send(req.query);
+});
+
+//PORT - enviornment variable.
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('listnening on port ' + port));
